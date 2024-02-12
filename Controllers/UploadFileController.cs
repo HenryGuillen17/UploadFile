f45a3105d854f864043ac7d91bdabfe2b05f9095
Henry Jesús Guillén Chacón <henry.guillen17@gmail.com>
2024-02-12 10:44:47 -0500

    Add project files.

---
+++ Controllers/UploadFileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UploadFile.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UploadFileController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> UploadFile([FromForm] FileUploadRequest request)
+    {
+        var file = request.File;
+
+        if (file.Length <= 0) return BadRequest("File Not found");
+
+        if (!string.Equals(Path.GetExtension(file.FileName), "*.xls", StringComparison.OrdinalIgnoreCase))
+            return BadRequest("File Not supported");
+
+        var filePath = "assets/" + file.FileName;
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        return Ok(new { message = "Archivo subido con éxito", filePath });
+    }
+}
+public record FileUploadRequest(IFormFile File);
